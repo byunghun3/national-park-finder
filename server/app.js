390aa8d265d0ec79cd37048d755ff81bd2948095
Byung-Hun Kim <byunghun3@gmail.com>
2022-05-30 08:25:02 -0400

    update cors

--- server/app.js
+++ server/app.js
@@ -15,6 +15,7 @@ app.use(function (req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "https://byunghun3.github.io/national-park-finder/");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Credentials", true);
     next();
 });
 
