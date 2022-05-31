176f9b27f4ebdd0512e6a29901237edd231cb9ad
Byung-Hun Kim <byunghun3@gmail.com>
2022-05-30 20:53:40 -0400

    remove unnecessary cors code

--- server/app.js
+++ server/app.js
@@ -7,22 +7,9 @@ const cors = require("cors");
 dotenv.config();
 
 app.use(express.json());
-
-const corsOptions = {
-    allowedHeaders: ["Content-Type", "authorization"],
-    origin: ["https://byunghun3.github.io/national-park-finder/"]
-};
-app.use(cors(corsOptions));
+app.use(cors());
 
 const usersRouter = require("./routes/users");
-
-app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    next();
-});
 
 app.use("/users", usersRouter);
 
